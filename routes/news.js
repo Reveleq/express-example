5e4ea06d4985579982fde3765d6daa82e15124e5
MacbookReveleq <damianw182@interia.pl>
2024-05-09 20:28:21 +0200

    news filter front

--- routes/news.js
+++ routes/news.js
@@ -1,9 +1,22 @@
 const express = require("express");
+const News = require("../models/news");
 const router = express.Router();
-
 /* GET home page. */
 router.get("/", (req, res) => {
-  res.render("news", { title: "News" });
+  console.log(req.query);
+  const search = req.query.search || "";
+  async function getNews() {
+    try {
+      const data = News.find({ title: new RegExp(search.trim(), "i") }).sort({
+        created: -1,
+      });
+      const results = await data;
+      await res.render("news", { title: "News", results, search });
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  getNews();
 });
 
 module.exports = router;
